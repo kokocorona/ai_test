7933e6162ce9c35d5f957e37191fcca7e41bc1fc
kokocorona <68634519+kokocorona@users.noreply.github.com>
2024-05-31 13:54:25 +0300

    Add files via upload

--- public/js/upload.js
+++ public/js/upload.js
@@ -7,11 +7,14 @@ const init = () => {
 const declareEvents = () => {
   const id_form = document.querySelector("#id_form");
   const id_file = document.querySelector("#id_file"); 
+  const id_loading = document.querySelector("#id_loading");
+  id_loading.style.display = "none";
   id_form.addEventListener("submit", async(e) => {
     e.preventDefault();
     try {
       console.log(id_file.files[0]);
       const form = new FormData();
+      id_loading.style.display = "block";
       form.append("foo",id_file.files[0] )
       // const url = "http://localhost:3001/ai/image";
       const url = "https://ai-test-p4ph.onrender.com/ai/image";
@@ -20,6 +23,7 @@ const declareEvents = () => {
         method:"POST",
         data:form
       })
+      id_loading.style.display = "none";
       console.log(resp.data);
       createList(resp.data)
     } catch (error) {
