414d5d6f26e9946688a151cd81765db89748ed1e
kokocorona <68634519+kokocorona@users.noreply.github.com>
2024-06-02 12:24:57 +0300

    Add files via upload

--- routes/configRoutes.js
+++ routes/configRoutes.js
@@ -13,9 +13,9 @@ exports.routesInit = (app) => {
   app.use("/categories", categoriesR);
   app.use("/ai", aiR);
 
-  // app.use("*",(req,res) => {
+  app.use((req,res) => {
 
-  //   res.sendFile(path.join(__dirname, '../public/index.html'))
-  //   // res.status(404).json({msg_error:"Url not found , 404!"})
-  // })
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+    // res.status(404).json({msg_error:"Url not found , 404!"})
+  })
 };
